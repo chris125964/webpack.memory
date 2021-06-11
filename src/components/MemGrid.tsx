0cd32d2e5c4d3ca55faf21786a0b78cbcb8600bf
Christian Todd <ct.admin@mnet-mail.de>
2021-06-11 22:54:27 +0200

    modified some CSS breakpoints

--- src/components/MemGrid.tsx
+++ src/components/MemGrid.tsx
@@ -29,20 +29,36 @@ export const MemGrid = ({}: TGridProps) => {
   };
 
   let size = 30;
+  let length = 20;
+  let headerHeight = 0;
   if (window.innerWidth <= 320) {
     // 320 x 496 -> size: 80px
     // 96 + (4 x 80) x (5 x 80)
     size = 20;
+    length = 80;
+    headerHeight = 96;
   } else if (window.innerWidth <= 600) {
     // 600 x 792 -> size: 120px
     // 72px + (5 x 120) x (6 x 120)
-    size = 16;
+    size = 30;
+    length = 120;
+    headerHeight = 72;
   } else if (window.innerWidth <= 640) {
     size = 20;
+    length = 120;
+    headerHeight = 50;
   } else if (window.innerWidth <= 960) {
     // 960 x 432 -> size: 120px
     // 72px  + (8 x 120) x (3 x 120)
-    size = 20;
+    size = 24;
+    length = 120;
+    headerHeight = 72;
+  } else if (window.innerWidth <= 1500) {
+    //  -> 150px
+    // 50px + (10 x 150) x (5 x 150)
+    size = 50;
+    length = 150;
+    headerHeight = 50;
   }
 
   let arr: number[] = [];
