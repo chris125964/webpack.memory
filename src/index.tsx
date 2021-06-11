7647fedfa5982388e4109d5ace3cd1f103d78b4f
Christian Todd <ct.admin@mnet-mail.de>
2021-06-11 22:22:47 +0200

    remove React warning

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 
-render(<App idx={1} />, document.body);
+render(<App idx={1} />, document.getElementById("root"));
