b1177c81b96cb720f79bb015be993190387432ec
Christian Todd <ct.admin@mnet-mail.de>
2021-06-11 22:25:18 +0200

    prparation for button events

--- src/components/MemGrid.tsx
+++ src/components/MemGrid.tsx
@@ -10,15 +10,38 @@ interface TGridProps {}
 
 export const MemGrid = ({}: TGridProps) => {
   const [showModal, setShowModal] = useState<boolean>(false);
+  const dispatch = () => {};
 
-  const renderTile = () => {
-    return <MemTile />;
+  const clickTile = (index: number, nr: number) => {
+    console.log(`clickTile`);
+  };
+
+  const renderTile = (index: number) => {
+    return (
+      <MemTile
+        key={index}
+        nr={index}
+        loop={index}
+        click={clickTile}
+        dispatch={dispatch}
+      />
+    );
   };
 
   let size = 30;
   if (window.innerWidth <= 320) {
+    // 320 x 496 -> size: 80px
+    // 96 + (4 x 80) x (5 x 80)
+    size = 20;
+  } else if (window.innerWidth <= 600) {
+    // 600 x 792 -> size: 120px
+    // 72px + (5 x 120) x (6 x 120)
     size = 16;
   } else if (window.innerWidth <= 640) {
+    size = 20;
+  } else if (window.innerWidth <= 960) {
+    // 960 x 432 -> size: 120px
+    // 72px  + (8 x 120) x (3 x 120)
     size = 20;
   }
 
@@ -28,8 +51,8 @@ export const MemGrid = ({}: TGridProps) => {
   }
 
   const renderTiles = () => {
-    return arr.map((ob) => renderTile());
+    return arr.map((ob, index) => renderTile(index));
   };
 
-  return <div>{renderTiles()}</div>;
+  return <div key={1}>{renderTiles()}</div>;
 };
--- src/components/MemTile.tsx
+++ src/components/MemTile.tsx
@@ -1,6 +1,29 @@
 import React from "react";
 import "./style.css";
+import { MemoryAction } from "../redux/memory/type";
 
-export const MemTile = () => {
-  return <button className="memtile">Tile</button>;
+interface MemTileProps {
+  loop: number;
+  nr: number;
+  click: (
+    index: number,
+    nr: number,
+    disp: (action: MemoryAction) => void
+  ) => void;
+  dispatch: (action: MemoryAction) => void;
+}
+
+export const MemTile = ({ loop, nr, click, dispatch }: MemTileProps) => {
+  const changeColor =
+    (loop: number, nr: number, dispatch: (action: MemoryAction) => void) =>
+    () => {
+      click(loop, nr, dispatch);
+    };
+  const clickFunc = changeColor(loop, nr, dispatch);
+
+  return (
+    <button key={nr} className="memtile" onClick={clickFunc}>
+      Tile
+    </button>
+  );
 };
