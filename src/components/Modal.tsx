b17dbf4826ce73c0476048fe5f21cbd5e8b5869b
Christian Todd <ct.admin@mnet-mail.de>
2021-10-26 22:54:24 +0200

    different final images

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,6 +14,19 @@ export const Modal = ({ show, onClose }: ModalProps) => {
 
   const img = 'landscape';
 
+  /**
+   * returns a random number between 0 and _max_
+   *
+   * @param {number} max
+   * @return {*}
+   */
+  const getRandomInt = (max: number) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  const finalNumber = '00' + getRandomInt(5).toString().slice(0);
+  console.log(`finalNumber: ${finalNumber}`);
+
   return (
     <div data-testid="modal.xxx" className={showHideClassName}>
       <button
@@ -21,7 +34,10 @@ export const Modal = ({ show, onClose }: ModalProps) => {
         data-testid="modal.close-button"
         onClick={onCloseWindow}
       >
-        <img className={img} src="./assets/images/1860-final-001.jpg" />
+        <img
+          className={img}
+          src={`./assets/images/1860-final-${finalNumber}.jpg`}
+        />
       </button>
     </div>
   );
